74c586a8df8bff4c96def13107e0b5acbff105f9
SadipBhattarai <sadipbhattarai3@gmail.com>
2026-05-18 19:59:14 +0545

    Added register and login funciton

---
+++ modules/users/user.controller.js
@@ -0,0 +1,6 @@
+const userModel = require("./user.model");
+
+const login = ({login, password}) => {};
+const register = (payload) => {};
+
+module.exports = { login, register };
---
+++ modules/users/user.model.js
@@ -0,0 +1,19 @@
+const { required } = require("joi");
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isBlocked: { type: Boolean, required: true, default: false },
+    isEmailVerified: { type: Boolean, required: true, default: false },
+    otp: { type: String },
+    roles: { type: [String], enum: ["user", "admin"], default: "user" },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports("Users", userSchema);
